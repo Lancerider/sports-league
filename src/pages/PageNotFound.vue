f63d1406b2b96e75692b23ebd04aeaf6deeb7fbc
Jorge Luis Palacios <jorgepalacioscl@gmail.com>
2022-06-07 12:21:38 -0400

    :sparkles: page not found ready

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="">
-    Error 404
+  <div class="not-found-container">
+    <img src="@/assets/images/404.png" alt="Page Not Found" class="error-image" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'PageNotFound',
-
 }
 </script>
 
 <style lang="scss" scoped>
+
+.not-found-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 60px 5% 20px 5%;
+}
 
 </style>
